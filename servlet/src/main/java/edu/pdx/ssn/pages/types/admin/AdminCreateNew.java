434c64305689753180f1ea88f9eab26124ab142e
Andrew Wen <2060andrew@gmail.com>
2015-05-22 14:20:47 -0700

    Finish catalog entry creation form

--- servlet/src/main/java/edu/pdx/ssn/pages/types/admin/AdminCreateNew.java
+++ servlet/src/main/java/edu/pdx/ssn/pages/types/admin/AdminCreateNew.java
@@ -1,7 +1,6 @@
 package edu.pdx.ssn.pages.types.admin;
 
 import edu.pdx.ssn.Server;
-import edu.pdx.ssn.application.Book;
 import edu.pdx.ssn.pages.ServerPage;
 
 import javax.servlet.http.HttpServletRequest;
@@ -21,25 +20,20 @@ public class AdminCreateNew implements ServerPage {
 
     @Override
     public void doPost(HttpServletRequest req) {
-        if (req.getParameter("confirm") != null) {
-
-        } else {
-            if (Boolean.valueOf(req.getParameter("confirm"))) { // Confirmed
-                long barcode = Long.valueOf(req.getParameter("code"));
-                long isbn = Long.valueOf(req.getParameter("isbn"));
-                String title = req.getParameter("title");
-                String last = req.getParameter("last");
-                String first = req.getParameter("first");
-                String profs = req.getParameter("profs");
-                String subj = req.getParameter("subj");
-                int num = Integer.valueOf(req.getParameter("num"));
-                long donor = Long.valueOf(req.getParameter("donor") == null ? "0" : req.getParameter("donor"));
-                long ret = Long.valueOf(req.getParameter("return") == null ? "0" : req.getParameter("return"));
-                new Book(barcode, isbn, title, last, first, profs, subj, num, donor, ret);
-                req.setAttribute("book", Server.getLibrary().createNew(barcode, isbn, title, last, first, profs, subj, num, donor, ret));
-            } else {
-                return;
-            }
+        if (req.getAttribute("confirm") == null) {
+            long barcode = Long.valueOf(req.getParameter("code"));
+            long isbn = Long.valueOf(req.getParameter("isbn"));
+            String title = req.getParameter("title");
+            String last = req.getParameter("last");
+            String first = req.getParameter("first");
+            String profs = req.getParameter("profs");
+            profs = profs.replaceAll(",", "::");
+            String subj = req.getParameter("subj");
+            int num = Integer.valueOf(req.getParameter("num"));
+            long donor = Long.valueOf(req.getParameter("donor") == null ? "0" : req.getParameter("donor"));
+            long ret = Long.valueOf(req.getParameter("return") == null ? "0" : req.getParameter("return"));
+            req.setAttribute("book", Server.getLibrary().createNew(barcode, isbn, title, last, first, profs, subj, num, donor, ret));
+            req.setAttribute("confirm", true);
         }
     }
 }
