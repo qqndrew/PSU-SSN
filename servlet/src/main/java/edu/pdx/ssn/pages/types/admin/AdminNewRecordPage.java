22d31533231b6503742b569182959be25d833c9d
Andrew Wen <2060andrew@gmail.com>
2015-05-25 21:54:03 -0700

    Compare parameter to "" instead of null

--- servlet/src/main/java/edu/pdx/ssn/pages/types/admin/AdminNewRecordPage.java
+++ servlet/src/main/java/edu/pdx/ssn/pages/types/admin/AdminNewRecordPage.java
@@ -22,25 +22,33 @@ public class AdminNewRecordPage implements ServerPage {
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp) {
         if (req.getAttribute("confirm") == null || req.getAttribute("confirm").equals(false)) {
-            long barcode = Long.valueOf(req.getParameter("code").replaceAll("[^0-9]", ""));
+            String temp = req.getParameter("code").replaceAll("[^0-9]", "");
+            if (temp.equals("")) {
+                temp = "0";
+            }
+            long barcode = Long.valueOf(temp);
             if (Server.getLibrary().getRecord(barcode) != null) {
                 req.setAttribute("err", "A record with this barcode already exists!");
                 req.setAttribute("confirm", false);
             }
-            long isbn = Long.valueOf(req.getParameter("isbn").replaceAll("[^0-9]", ""));
+            temp = req.getParameter("isbn").replaceAll("[^0-9]", "");
+            if (temp.equals("")) {
+                temp = "0";
+            }
+            long isbn = Long.valueOf(temp);
             if (Server.getLibrary().getBook(isbn) == null) {
                 req.setAttribute("err", "The book with the provided isbn " + isbn + " does not exist in this system");
                 req.setAttribute("confirm", false);
             }
-            String temp = req.getParameter("donor");
+            temp = req.getParameter("donor");
             Long donorUid = null;
-            if (temp != null) {
+            if (!temp.equals("")) {
                 donorUid = Long.valueOf(temp.replaceAll("[^0-9]", ""));
             }
-            String ret = req.getParameter("return");
+            temp = req.getParameter("return");
             Long retDate = null;
-            if (ret != null) {
-                retDate = Long.valueOf(ret);
+            if (!temp.equals("")) {
+                retDate = Long.valueOf(temp);
             }
             Record record = Server.getLibrary().createRecord(barcode, isbn, donorUid, retDate);
             req.setAttribute("record", record);
