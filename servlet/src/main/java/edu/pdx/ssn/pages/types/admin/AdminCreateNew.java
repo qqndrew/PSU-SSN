151797569fae25130dae9834ca5afe3e575d9d6c
Andrew Wen <2060andrew@gmail.com>
2015-05-27 23:08:55 -0700

    ISBN input validation

--- servlet/src/main/java/edu/pdx/ssn/pages/types/admin/AdminCreateNew.java
+++ servlet/src/main/java/edu/pdx/ssn/pages/types/admin/AdminCreateNew.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 public class AdminCreateNew implements ServerPage {
 
 
+
     @Override
     public boolean processRequest(HttpServletRequest req, HttpServletResponse resp) {
         req.setAttribute("err", "");
@@ -27,7 +28,15 @@ public class AdminCreateNew implements ServerPage {
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp) {
         if (req.getAttribute("confirm") == null || req.getAttribute("confirm").equals(false)) {
-            long isbn = Long.valueOf(req.getParameter("isbn").replaceAll("[^0-9]", ""));
+            long isbn = 0;
+            try {
+                isbn = Long.valueOf(req.getParameter("isbn").replaceAll("[^0-9]", ""));
+            } catch (NumberFormatException e) {
+                req.setAttribute("err", "This is not a valid isbn!");
+                req.setAttribute("confirm", false);
+                forward(req, resp);
+                return;
+            }
             String title = req.getParameter("title");
             String last = req.getParameter("last");
             String first = req.getParameter("first");
@@ -44,15 +53,19 @@ public class AdminCreateNew implements ServerPage {
                 req.setAttribute("err", "");
                 req.setAttribute("forward", "Book successfully created!");
             }
-            try {
-                PageManager.getPage("admin").setMetaAttributes(req);
-                req.setAttribute("admpage", "create_new");
-                req.getRequestDispatcher("/WEB-INF/index.jsp?app=admin&page=create_new").forward(req, resp);
-            } catch (ServletException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            forward(req, resp);
+        }
+    }
+
+    private void forward(HttpServletRequest req, HttpServletResponse resp) {
+        try {
+            PageManager.getPage("admin").setMetaAttributes(req);
+            req.setAttribute("admpage", "create_new");
+            req.getRequestDispatcher("/WEB-INF/index.jsp?app=admin&page=create_new").forward(req, resp);
+        } catch (ServletException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
