bb88f05cd12dd682e32a1b131182d19de0fadc37
Andrew Wen <2060andrew@gmail.com>
2015-05-28 13:20:51 -0700

    Add member page to page registry

--- servlet/src/main/java/edu/pdx/ssn/pages/PageManager.java
+++ servlet/src/main/java/edu/pdx/ssn/pages/PageManager.java
@@ -7,6 +7,7 @@ import edu.pdx.ssn.pages.types.DetailsPage;
 import edu.pdx.ssn.pages.types.IndexPage;
 import edu.pdx.ssn.pages.types.LoginPage;
 import edu.pdx.ssn.pages.types.LogoutPage;
+import edu.pdx.ssn.pages.types.MemberPage;
 import edu.pdx.ssn.pages.types.RegisterPage;
 
 import java.util.HashMap;
@@ -30,6 +31,7 @@ public class PageManager {
         pages.put(DetailsPage.PAGE_KEY, new DetailsPage());
         pages.put(LoginPage.PAGE_KEY, new LoginPage());
         pages.put(RegisterPage.PAGE_KEY, new RegisterPage());
+        pages.put(MemberPage.PAGE_KEY, new MemberPage());
     }
 
     public static ServerPage getPage(String key) {
